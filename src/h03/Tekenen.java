51e29b47e73747e5a0e504d4ae1213de02530652
honman21 <hdeong@roc-dev.com>
2019-09-04 11:22:56 +0200

    Tekenen

---
+++ src/h03/Tekenen.java
@@ -0,0 +1,17 @@
+package h03;
+
+import java.awt.*;
+import java.applet.*;
+
+    public class Tekenen extends Applet {
+
+        public void init() {
+        }
+
+        public void paint(Graphics g) {
+            g.drawLine(80, 30,140,140);
+            g.drawLine(30, 140, 80, 30);
+            g.drawLine(30, 140, 140, 140);
+        }
+    }
+
