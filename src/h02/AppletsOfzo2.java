0b614508453d41ff331c87a554141425043169f9
honman21 <hdeong@roc-dev.com>
2019-09-04 11:29:42 +0200

    Tekenen2

--- src/h02/AppletsOfzo2.java
+++ src/h02/AppletsOfzo2.java
@@ -1,20 +1,21 @@
 package h02;
 
-        import java.awt.*;
-        import java.applet.*;
+import java.awt.*;
+import java.applet.*;
 
 //een klasse met de naam Show van het type Applet
-public class AppletsOfzo extends Applet {
+public class AppletsOfzo2 extends Applet {
 
     //een (lege) methode die de Applet initialiseert
     public void init() {
-        setBackground(Color.blue);
+        setBackground(Color.WHITE);
     }
 
     //een methode die de inhoud van het scherm tekent
     public void paint(Graphics g) {
-        g.setColor(Color.yellow);
+        g.setColor(Color.BLUE);
         g.drawString("Honman", 50, 60);
-
+        g.setColor(Color.RED);
+        g.drawString("Deong", 50, 70);
     }
 }
