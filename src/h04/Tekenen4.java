699e2ff6865577a98ed9a20cf402e70e0c33207b
honman21 <hdeong@roc-dev.com>
2019-09-06 12:36:34 +0200

    Tekenen4

--- src/h04/Tekenen4.java
+++ src/h04/Tekenen4.java
@@ -9,28 +9,44 @@ public class Tekenen4 extends Applet {
     }
 
     public void paint(Graphics g) {
+        //strepen
+        g.drawLine(68,140,68,340);
+        g.drawLine(68,140,230,140);
+        g.drawLine(230,340,230,140);
+        g.drawLine(230,340,68,340);
+        g.drawLine(68,300,230,300);
+        g.drawLine(68,260,230,260);
+        g.drawLine(68,220,230,220);
+        g.drawLine(68,180,230,180);
+
+        //staven
         g.setColor(Color.pink);
-        g.fillRect(80, 130, 40, 40);
+        g.fillRect(80, 260, 40, 80);
         g.setColor(Color.blue);
-        g.fillRect(120, 70, 40, 100);
+        g.fillRect(130, 140, 40, 200);
         g.setColor(Color.cyan);
-        g.fillRect(160, 90, 40, 80);
+        g.fillRect(180, 180, 40, 160);
+
+        //namen
         g.setColor(Color.black);
-        g.drawString("Valerie", 80, 180);
+        g.drawString("Valerie", 80, 350);
         g.setColor(Color.black);
-        g.drawString("Jeroen", 120, 180);
+        g.drawString("Jeroen", 130, 350);
         g.setColor(Color.black);
-        g.drawString("Hans", 160, 180);
+        g.drawString("Hans", 180, 350);
         g.setColor(Color.black);
-        g.drawString("20KG", 30, 160);
+
+
+        //kg
+        g.drawString("20KG", 30, 300);
         g.setColor(Color.black);
-        g.drawString("40KG", 30, 140);
+        g.drawString("40KG", 30, 260);
         g.setColor(Color.black);
-        g.drawString("60KG", 30, 120);
+        g.drawString("60KG", 30, 220);
         g.setColor(Color.black);
-        g.drawString("80KG", 30, 100);
+        g.drawString("80KG", 30, 180);
         g.setColor(Color.black);
-        g.drawString("100KG", 30, 80);
+        g.drawString("100KG", 30, 140);
 
 
 
